ce14d7589925b6969f3f712f26672349e5ad0c25
Nate <nate@natedpalm.com>
2020-05-05 20:14:17 -0400

    Initial commit

---
+++ src/BattletechBuilder.Data/Common.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace BattletechBuilder.Data
+{
+	public enum TechBase
+	{
+		InnerSphere,
+		Clan
+	}
+	public enum Configuration
+	{
+		Biped,
+		Quad
+	}
+	public enum TechnicalReadout
+	{
+		TR2750,
+		TR3025,
+		TR3026,
+		TR3039,
+		TR3050,
+		TR3055,
+		TR3057,
+		TR3058,
+		TR3060,
+		TR3067,
+		TR3085,
+		TR3145Capellan,
+		TR3145Clans,
+		TR3145Draconis,
+		TR3145Federated,
+		TR3145Lyran,
+		TR3145FreeWorlds,
+		TR3145Mercs,
+		TR3145,
+		TR3145Republic,
+		TR3150,
+		TRClanInvasion,
+		XTRO
+
+	}
+}
---
+++ src/BattletechBuilder.Data/Mech.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BattletechBuilder.Data
+{
+	public class Mech
+	{
+		public string Name { get; set; }
+		public string Variant { get; set; }
+		public
+	}
+}
